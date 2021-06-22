4f25054a2e68469e48d2191e712659fd4a2a75a7
Elif Çürük <67744182+elfcurk@users.noreply.github.com>
2021-06-22 22:56:31 +0300

    Update JLoggerManager.java

--- nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
+++ nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -2,6 +2,6 @@ package nLayeredDemo.jLogger;
 
 public class JLoggerManager {
 	public void log(String message) {
-		System.out.println("J Logger ile loglandý, " +message);
+		System.out.println("J Logger ile loglandı, " + message);
 	}
 }
